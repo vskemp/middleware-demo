d93fd59b768ed53936d27c43840db025929e2629
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-01-07 11:41:16 -0500

    helmet and morgan introduced

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const http = require("http");
 const app = express();
 const PORT = 3000;
 const server = http.createServer(app);
+const morgan = require("morgan");
+const logger = morgan("tiny");
+const helmet = require("helmet");
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next(); // send signal to exspress telling it that this middleware is done
-});
+app.use(logger);
+app.use(helmet());
+
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next(); // send signal to exspress telling it that this middleware is done
+// });
 
 app.get("/", (req, res) => {
     res.send("<h1>Woo! It's home page!</h1>");
-    // next();
+    // next()
 });;
 
 server.listen(PORT, () => {
